0d1e377c4b9873ed2c3b75416d908e338a996d9d
2Gpb <Pvtyukhmenev@gmail.com>
2025-03-08 12:32:14 +0300

    initial commit

---
+++ DesignSystem-IT-ch/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  DesignSystem-IT-ch
+//
+//  Created by Peter on 08.03.2025.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
